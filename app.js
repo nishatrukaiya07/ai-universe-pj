4121bb711e57ff65edba094af3128d446701690b
nishatrukaiya07 <nishatrukaiya07@gmail.com>
2023-04-05 10:39:01 +0600

    edit-card

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+let allData =[]
 const loadData = async() =>{
   const url =`https://openapi.programming-hero.com/api/ai/tools`
   const res = await fetch(url);
   const data = await res.json();
+  allData = data.data.tools;
   displayData(data.data.tools);
 }
 
@@ -16,12 +18,25 @@ tools.forEach(tool =>{
   toolDiv.innerHTML=`
   <div class="card h-100">
     <img src="${tool.image}" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">${tool.name}</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <div class="card-body ">
+      <h5 class="card-title">Features</h5>
+      <ol>
+      <li>Natural language processing</li>
+      <li> Contextual understanding</li>
+      <li>Text generation</li>
+      </ol>
     </div>
-    <div class="card-footer">
-      <small class="text-body-secondary">Last updated 3 mins ago</small>
+    <div class="card-footer border-0 bg-body">
+       <h5 class="card-title">${tool.name}</h5>
+      <div class="d-flex justify-content-between">
+        <div class="d-flex gap-2 align-items-center">
+          <i class="fa-solid fa-calendar-days m-0 p-0 "></i>
+          <p class="m-0 p-0">11/01/2022</p>
+        </div>
+        <div class = "bg-danger-subtle px-3 py-3 rounded-5 text-danger mb-4">
+          <i class ="fas fa-arrow-right"></i>
+        </div>
+     </div>
     </div>
   </div>
   `
@@ -32,3 +47,4 @@ toolsContainer.appendChild(toolDiv);
 
 
 loadData();
+
