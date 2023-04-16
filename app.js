8401dfe01fc2f0685aabc251417fe1e2585b465b
nishatrukaiya07 <nishatrukaiya07@gmail.com>
2023-04-16 21:38:54 +0600

    edit card

--- app.js
+++ app.js
@@ -16,11 +16,11 @@ tools.forEach(tool =>{
   const toolDiv = document.createElement('div');
   toolDiv.classList.add('col');
   toolDiv.innerHTML=`
-  <div class="card h-100  rounded">
-    <img src="${tool.image}" class="card-img-top p-3 " alt="...">
+  <div class="card h-100 px-3 py-3 bg-white">
+    <img class="img-fluid rounded-4 mb-3" src="${tool.image}" class="card-img-top p-3 " alt="...">
     <div class="card-body bg-body ">
-      <h5 class="card-title">Features</h5>
-      <ol>
+      <h5 class="card-title fw-bolder">Features</h5>
+      <ol class="text-secondary">
       <li>Natural language processing</li>
       <li> Contextual understanding</li>
       <li>Text generation</li>
@@ -28,11 +28,11 @@ tools.forEach(tool =>{
     </div>
     <hr class="mx-4">
     <div class="card-footer border-0 bg-body">
-       <h5 class="card-title">${tool.name}</h5>
+       <h5 class="card-title fw-bolder">${tool.name}</h5>
       <div class="d-flex justify-content-between">
         <div class="d-flex gap-2 align-items-center">
-          <i class="fa-solid fa-calendar-days m-0 p-0 "></i>
-          <p class="m-0 p-0">11/01/2022</p>
+          <i class="fa-solid fa-calendar-days text-secondary m-0 p-0 "></i>
+          <p class="m-0 p-0 text-secondary">11/01/2022</p>
         </div>
         <div class = "bg-danger-subtle opacity-50  text-danger px-3 py-3 rounded-5 mb-4">
           <i class ="fas fa-arrow-right " onclick = "fetchShowDetails('${tool.id}')"data-bs-toggle="modal" data-bs-target="#toolDetailsModal"></i>
